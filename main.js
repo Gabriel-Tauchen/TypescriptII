afd2e2041f91b1ee68746b640093ce5d81e8ccd4
Gabriel <tauchen.gabriel@gmail.com>
2025-03-28 16:26:07 -0300

    Classe

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
 //Classe
-
 // class Pessoa {
 //     nome: string;
 //     idade: number;
@@ -11,9 +10,7 @@
 // pessoa.nome = "John Doe";
 // pessoa.idade = 30;
 // pessoa.apresentar();
-
 //Visibilidade
-
 // class Conta{
 //     public nome: string;
 //     private saldo: number;
@@ -25,33 +22,25 @@
 //         console.log(`Saldo: ${this.saldo}`)
 //     }
 // }
-
 // const conta = new Conta("Conta Corrente", 1000000);
 // console.log(conta.nome);
 // //console.log(conta.saldo); //erro
 // conta.mostrarSaldo();
-
 //Propriedade sem uso de construtor
-
 // class Produto{
 //     public nome: string = "Produto";
 //     public preco: number = 0;
 // }
-
 // const note = new Produto();
 // console.log(note.nome);
-
 //Propriedade com construtor
-
 // class Produto {
 //     constructor(public nome: string,
 //     public preco: number){}
 //     }
 //     const note = new Produto("Notebook", 2500);
 //     console.log(note.nome()); //dá erro pois não estão inicializados os parâmetros
-
 //Valor Padrão da propriedade no construtor
-
 // class Produto {
 //     constructor(public nome: string = '',
 //     public preco: number = 0){}
@@ -62,13 +51,10 @@
 // console.log(note.nome);
 // note.preco=3000;
 // console.log(note.preco);
-
 //Gets e Sets
-
 // class Produto {
 //         constructor(public nome: string,
 //         private _preco: number){}
-
 //         get preco() : number{
 //             console.log("get");
 //             return this._preco;
@@ -84,16 +70,17 @@
 //     console.log(note.nome);
 //     note.preco=3000;//invoca o set
 //     console.log(note.preco);//invoca o get
-
 // Propriedade opcional
-
-class Produto {
-    constructor(public nome: string,
-    public preco?: number){}
-}
-const note = new Produto("Notebook", 2500);
+var Produto = /** @class */ (function () {
+    function Produto(nome, preco) {
+        this.nome = nome;
+        this.preco = preco;
+    }
+    return Produto;
+}());
+var note = new Produto("Notebook", 2500);
 console.log(note.nome);
-note.preco=3000;
-const note1 = new Produto("Notebook 1");
+note.preco = 3000;
+var note1 = new Produto("Notebook 1");
 console.log(note1.preco);
-note1.preco=10;
+note1.preco = 10;
